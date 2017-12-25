abc2935eba996ba91635c37f4cc3de19f38ed298
Dirk Eddelbuettel <edd@debian.org>
2017-12-25 16:09:26 -0600

    also commit local/setup.R

---
+++ local/setup.R
@@ -0,0 +1,12 @@
+
+options("width"=100,
+        "digits.secs"=2)
+
+r <- character()
+r["CRAN"] <- "http://cran.rstudio.com"
+#r["BioCsoft"] <- "http://www.bioconductor.org/packages/release/bioc"
+options(repos = r)
+
+if (exists("opt") && "exclset" %in% names(opt) && opt$exclset == "") {
+    opt$exclset <- "~/git/rcpp-log/data/blacklist.csv"
+}
