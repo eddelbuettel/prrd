9aaa13127126911d5ea7b9dad3c63a93c50613f8
Dirk Eddelbuettel <edd@debian.org>
2017-12-27 09:13:26 -0600

    enhanced local setup.R

--- local/setup.R
+++ local/setup.R
@@ -7,8 +7,12 @@ r["CRAN"] <- "http://cran.rstudio.com"
 #r["BioCsoft"] <- "http://www.bioconductor.org/packages/release/bioc"
 options(repos = r)
 
-if (exists("opt") && "exclset" %in% names(opt) && opt$exclset == "") {
-    opt$exclset <- "~/git/rcpp-log/data/blacklist.csv"
+if (exists("opt") && "exclude" %in% names(opt) && opt$exclude == "") {
+    opt$exclude <- "~/git/rcpp-logs/data/blacklist.csv"
 }
 
 Sys.setenv("MAKE"="make")
+
+Sys.setenv("BOOSTLIB"="/usr/include")   # for the borked src/Makevars of ExactNumCI
+Sys.setenv("RGL_USE_NULL"="TRUE")       # Duncan Murdoch on r-package-devel on 12 Aug 2015#
+Sys.setenv("R_MAKEVARS_USER"="~/git/rcpp-logs/data/dot.Makevars")
