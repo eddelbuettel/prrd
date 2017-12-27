86e14e9baa709e5fdcb9f3b65f40a8c1159f750d
Dirk Eddelbuettel <edd@debian.org>
2017-12-27 16:35:28 -0600

    db watch script simplification

--- inst/scripts/watchDB.sh
+++ inst/scripts/watchDB.sh
@@ -2,8 +2,5 @@
 
 test -f $1 || exit 0
 
-cat <<EOF | sqlite3 $1
-.mode column
-.header on 
-select * from results order by endtime limit 10
-EOF
+sqlite3 -column -header $1 "select * from results order by endtime desc limit 10"
+
