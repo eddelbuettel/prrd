c0a9d9708fe46172a22cc9b2bcb5be6aec776080
Dirk Eddelbuettel <edd@debian.org>
2021-08-18 23:03:19 -0500

    support dbfile in script

--- inst/scripts/enqueueJobs.r
+++ inst/scripts/enqueueJobs.r
@@ -1,13 +1,16 @@
 #!/usr/bin/env r
 
 ## configuration for docopt
-doc <- paste0("Usage: enqueueJobs.r [-q QUEUE] [-h] [-x] PACKAGE
+doc <- paste0("[Usage: enqueueJobs.r [-q QUEUE] [-d DBFILE] [-h] [-x] PACKAGE
 
 -q --queue QUEUE      set queue directory [default: ", getOption("prrd.queue_directory", "."), "]
+-d --dbfile DBFILE    set database for prior run, used new packages only [default: ]
 -h --help             show this help text
 -x --usage            show help and short example usage")
 
 opt <- docopt::docopt(doc)              # docopt parsing
+
+if (is.null(opt$dbfile)) opt$dbfile <- ""
 
 if (opt$usage) {
     cat(doc, "\n\n")
